955e649ae0025994b631963b064a7eb266004ae7
Carlo Sciolla <carlo.sciolla@gmail.com>
2016-12-09 14:37:32 +0100

    one extra api

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker run -it -p 3000:3000 \
-               -v `pwd`:/opt/code/payment-clj
+               -v `pwd`:/opt/code/payment-clj \
               sockshop/payment-clj
--- src/foobar/handler.clj
+++ src/foobar/handler.clj
@@ -1,10 +1,24 @@
 (ns foobar.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.util.response :as resp]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn my-handler
+  [req]
+  (resp/content-type
+    (resp/response "I'm here")
+    "text/plain"))
+
+(defn my-another-handler
+  [req]
+  (resp/content-type
+    (resp/response "I'm also here")
+    "text/plain"))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] my-handler)
+  (GET "/another" [] my-another-handler)
   (route/not-found "Not Found"))
 
 (def app
